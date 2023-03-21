a19d5323ec4551cf365cc7fd91a03e504366a0cb
Fernando Dias <56140193+nand0diaz@users.noreply.github.com>
2023-03-21 14:17:56 -0300

    Upload to IPFS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,14 @@
-import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "../styles/globals.css";
+import type { AppProps } from "next/app"
+import { ThirdwebProvider } from "@thirdweb-dev/react"
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "mumbai"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThirdwebProvider activeChain={activeChain}>
-      <Component {...pageProps} />
-    </ThirdwebProvider>
-  );
+	return (
+		<ThirdwebProvider activeChain={activeChain}>
+			<Component {...pageProps} />
+		</ThirdwebProvider>
+	)
 }
 
-export default MyApp;
+export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,37 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import { useStorageUpload } from "@thirdweb-dev/react"
+import type { NextPage } from "next"
+import { useState } from "react"
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
+	const [file, setFile] = useState<File | null>(null)
 
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
+	const { mutateAsync: upload } = useStorageUpload()
 
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
+	const uploadToIpfs = async () => {
+		const uploadUrl = await upload({
+			data: [file],
+			options: {
+				uploadWithGatewayUrl: true,
+				uploadWithoutDirectory: true,
+			},
+		})
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
+		alert("Uploaded to IPFS: " + uploadUrl)
+	}
 
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
+	return (
+		<div>
+			<input
+				type="file"
+				onChange={(e) => {
+					if (e.target.files) {
+						setFile(e.target.files[0])
+					}
+				}}
+			/>
+			<button onClick={uploadToIpfs}>Upload</button>
+		</div>
+	)
+}
 
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
-  );
-};
-
-export default Home;
+export default Home
